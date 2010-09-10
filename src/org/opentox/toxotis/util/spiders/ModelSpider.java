1839ec9096daee421ad2908e276e42ec41b8bed3
Haralampos Homenidis <hampos@yahoo.com>
2010-09-10 17:30:22 +0300

    populating model and model spider

---
+++ src/org/opentox/toxotis/util/spiders/ModelSpider.java
@@ -0,0 +1,64 @@
+package org.opentox.toxotis.util.spiders;
+
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.Resource;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.opentox.toxotis.ToxOtisException;
+import org.opentox.toxotis.client.GetClient;
+import org.opentox.toxotis.client.VRI;
+import org.opentox.toxotis.core.Model;
+
+/**
+ *
+ * @author Charalampos Chomenides
+ * @author Pantelis Sopasakis
+ */
+public class ModelSpider extends Tarantula<Model>{
+
+    VRI uri;
+
+    public ModelSpider(VRI uri) throws ToxOtisException {
+        super();
+        this.uri = uri;
+        GetClient client = new GetClient();
+        client.setMediaType("application/rdf+xml");
+        client.setUri(uri);
+        model = client.getResponseOntModel();
+        resource = model.getResource(uri.toString());
+    }
+
+    public ModelSpider(Resource resource, OntModel model) {
+        super(resource, model);
+        try {
+            uri = new VRI(resource.getURI());
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(FeatureSpider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public ModelSpider(OntModel model, String uri) {
+        super();
+        this.model = model;
+        try {
+            this.uri = new VRI(uri);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(FeatureSpider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.resource = model.getResource(uri);
+    }
+
+    @Override
+    public Model parse() throws ToxOtisException {
+        Model m = new Model();
+
+        m.setUri(uri);
+        m.setMeta(new MetaInfoSpider(resource, model).parse());
+
+        
+
+        return m;
+    }
+
+}
