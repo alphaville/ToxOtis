78ce3b4a1b50d20a862c7b37790028910f003f49
Sopasakis Pantelis <chvng@mail.ntua.gr>
2010-09-28 16:22:57 +0300

    Compound updated

--- src/org/opentox/toxotis/core/Compound.java
+++ src/org/opentox/toxotis/core/Compound.java
@@ -2,6 +2,10 @@ package org.opentox.toxotis.core;
 
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.RDF;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -23,6 +27,10 @@ import org.opentox.toxotis.client.GetClient;
 import org.opentox.toxotis.client.PostClient;
 import org.opentox.toxotis.client.VRI;
 import org.opentox.toxotis.client.collection.Media;
+import org.opentox.toxotis.ontology.collection.OTClasses;
+import org.opentox.toxotis.util.aa.AuthenticationToken;
+import org.opentox.toxotis.util.spiders.DatasetSpider;
+import org.opentox.toxotis.util.spiders.TypedValue;
 import org.opentox.toxotis.client.collection.Services;
 import org.opentox.toxotis.util.aa.AuthenticationToken;
 import org.opentox.toxotis.util.spiders.CompoundSpider;
@@ -32,7 +40,7 @@ import org.opentox.toxotis.util.spiders.CompoundSpider;
  * and defined chemical structure up to its 2D characteristics. Also provides access
  * to different representations of the compound such as its SMILES or SDF formats.
  * Allows users to easily download these representations from remote locations and
- * store them locally in files. Also allows such representations to be POSTed to
+ * store them locally in files. Also allows such representations to be POSTed to 
  * remote services and new compounds to be created (published) online.
  *
  * @author Pantelis Sopasakis
@@ -40,9 +48,6 @@ import org.opentox.toxotis.util.spiders.CompoundSpider;
  */
 public class Compound extends OTPublishable<Compound> {
 
-    /*
-     * SHOULD THERE BE A CONFORMER URI IN THE COMPOUND?
-     */
     /**
      * Construct a new compound identified by its URI. You should provide a
      * valid compound URI. Conformer URIs are not acceptable in this constructor.
@@ -74,8 +79,11 @@ public class Compound extends OTPublishable<Compound> {
     }
 
     /**
-     * Return the identifier of the compound as it is extracted from its URI. For
+     * Return the (local) identifier of the compound as it is extracted from its URI. For
      * example http://someserver.com/compound/phenol will return <code>phenol</code>.
+     * When applied on a conformer, it will also return the (local) id of the
+     * compound. For example, if applied on <code>http://someserver.com/compound/1/conformer/2</code>,
+     * the method returns <code>1</code>.
      * @return
      *      Compound ID.
      */
@@ -84,6 +92,11 @@ public class Compound extends OTPublishable<Compound> {
         if (uriFragments.length == 2) {
             if (!uriFragments[1].contains("/conformer/")) {
                 return uriFragments[1];
+            } else {
+                String[] subFragments = uriFragments[1].split("/conformer/");
+                if (subFragments != null && subFragments.length > 0) {
+                    return subFragments[0];
+                }
             }
         }
         return null;
@@ -206,15 +219,12 @@ public class Compound extends OTPublishable<Compound> {
             /* FILE STREAM */
             FileWriter fileWriter = new FileWriter(destination);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
             String line = null;
             while ((line = remoteReader.readLine()) != null) {
                 bufferedWriter.write(line);
                 bufferedWriter.newLine();
             }
-
             Throwable failure = null;
-
             if (remote != null) {
                 try {
                     remote.close();
@@ -222,7 +232,6 @@ public class Compound extends OTPublishable<Compound> {
                     failure = th;
                 }
             }
-
             if (isr != null) {
                 try {
                     isr.close();
@@ -230,7 +239,6 @@ public class Compound extends OTPublishable<Compound> {
                     failure = th;
                 }
             }
-
             if (remoteReader != null) {
                 try {
                     remoteReader.close();
@@ -238,8 +246,6 @@ public class Compound extends OTPublishable<Compound> {
                     failure = th;
                 }
             }
-
-
             if (bufferedWriter != null) {
                 try {
                     bufferedWriter.flush();
@@ -248,7 +254,6 @@ public class Compound extends OTPublishable<Compound> {
                     failure = th;
                 }
             }
-
             if (failure != null) {
                 if (failure instanceof IOException) {
                     throw new ToxOtisException(ErrorCause.StreamCouldNotClose, failure);
@@ -256,10 +261,85 @@ public class Compound extends OTPublishable<Compound> {
                     throw new RuntimeException(failure);
                 }
             }
-
         } catch (IOException ex) {
             throw new ToxOtisException("Remote stream from '" + newUri.getStringNoQuery() + "' is not readable!", ex);
         }
+    }
+
+    /**
+     * Retrieves a property as a typed value, for a given feature, from the remote
+     * server that hosts the underlying compound.
+     * @param feature
+     *      Feature for which the value is retrieved
+     * @param
+     *      Token used to authenticate the client and authorize it to perform the
+     *      GET request to the remote servrer. If you think that no authentication
+     *      is needed to access the resource, you may set it to <code>null</code>.
+     * @return
+     *      Feature value for this compound as a Typed Value
+     * @throws ToxOtisException
+     *      In case an authentication error occurs or the remote service responds
+     *      with an error code like 500 or 503 or the submitted representation is
+     *      syntactically or semantically wrong (status 400).
+     */
+    public TypedValue<?> getProperty(Feature feature, AuthenticationToken token) throws ToxOtisException {
+        /**
+         *TODO: Should this request include the uri parameters for the feature?
+         */
+        VRI dsUri = new VRI(getUri()).addUrlParameter("feature_uris[]", feature.getUri().toString());
+        if (token != null) {
+            dsUri.addUrlParameter("tokenid", token.stringValue());
+        }
+        GetClient client = new GetClient();
+        client.setUri(dsUri);
+        client.setMediaType("application/rdf+xml");
+        OntModel model = client.getResponseOntModel();
+        RDF.type.inModel(model);
+        StmtIterator dsIt = model.listStatements(null, RDF.type, (RDFNode) OTClasses.Dataset().inModel(model));
+        Resource baseResource = null;
+        if (dsIt.hasNext()) {
+            baseResource = dsIt.nextStatement().getSubject();
+            DatasetSpider dsSpider = new DatasetSpider(baseResource, model);
+            Dataset ds = dsSpider.parse();
+            List<DataEntry> data = ds.getDataEntries();
+            if (data != null && data.size() >= 1) {
+                DataEntry firstEntry = ds.getDataEntries().get(0);
+                if (firstEntry != null && firstEntry.getFeatureValues().size() >= 1) {
+                    FeatureValue fVal = firstEntry.getFeatureValue(0);
+                    if (fVal != null) {
+                        return fVal.getValue();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public TypedValue getProperty(VRI uri, AuthenticationToken token) throws ToxOtisException {
+        Feature tempFeat = new Feature(uri);
+        return getProperty(tempFeat, token);
+    }
+
+    public Dataset getProperties(AuthenticationToken token, VRI... featureUris) throws ToxOtisException {
+        VRI dsUri = new VRI(getUri());
+        for (VRI featureUri : featureUris) {
+            dsUri.addUrlParameter("feature_uris[]", featureUri.toString());
+        }
+        if (token != null) {
+            dsUri.addUrlParameter("tokenid", token.stringValue());
+        }
+        GetClient client = new GetClient();
+        client.setUri(dsUri);
+        client.setMediaType("application/rdf+xml");
+        OntModel model = client.getResponseOntModel();
+        StmtIterator dsIt = model.listStatements(null, RDF.type, (RDFNode) OTClasses.Dataset().inModel(model));
+        Resource baseResource = null;
+        if (dsIt.hasNext()) {
+            baseResource = dsIt.nextStatement().getSubject();
+            DatasetSpider dsSpider = new DatasetSpider(baseResource, model);
+            return dsSpider.parse();
+        }
+        return null;
     }
 
     @Override
@@ -280,5 +360,20 @@ public class Compound extends OTPublishable<Compound> {
     @Override
     public Task publishOnline(AuthenticationToken token) throws ToxOtisException {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Set<VRI> listAvailableFeatures() throws ToxOtisException {
+        VRI featuresUri = new VRI(uri).augment("feature");
+        GetClient client = new GetClient();
+        client.setUri(featuresUri);
+        Set<VRI> availableUris = new HashSet<VRI>();
+        for (String fUri : client.getResponseUriList()) {
+            try {
+                availableUris.add(new VRI(fUri));
+            } catch (URISyntaxException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        return availableUris;
     }
 }
