8104f4f69dfb67070609e6d710188e3a5b42640e
Sopasakis Pantelis <chvng@mail.ntua.gr>
2011-01-03 15:09:18 +0200

    OTRestObjectProperties updated

--- src/org/opentox/toxotis/ontology/collection/OTRestObjectProperties.java
+++ src/org/opentox/toxotis/ontology/collection/OTRestObjectProperties.java
@@ -18,6 +18,7 @@ public class OTRestObjectProperties {
     private static OTObjectProperty ms_paramContent;
     private static OTObjectProperty ms_resource;
     private static OTObjectProperty ms_result;
+    private static OTObjectProperty ms_status;
     private static OTObjectProperty ms_uri;
 
     public static OTObjectProperty hasHTTPMethod() {
@@ -50,4 +51,44 @@ public class OTRestObjectProperties {
         }
         return ms_result;
     }
+
+    public static OTObjectProperty paramContent() {
+        if (ms_paramContent == null) {
+            OTObjectProperty clazz = new OTObjectPropertyImpl("paramContent", NS);
+            clazz.getDomain().add(OTRestClasses.InputParameter());
+            clazz.getDomain().add(OTClasses.OpenToxResource());
+            ms_paramContent = clazz;
+        }
+        return ms_paramContent;
+    }
+
+    public static OTObjectProperty resource() {
+        if (ms_resource == null) {
+            OTObjectProperty clazz = new OTObjectPropertyImpl("resource", NS);
+            clazz.getDomain().add(OTRestClasses.RESTOperation());
+            clazz.getDomain().add(OTClasses.OpenToxResource());
+            ms_resource = clazz;
+        }
+        return ms_resource;
+    }
+
+    public static OTObjectProperty status() {
+        if (ms_status == null) {
+            OTObjectProperty clazz = new OTObjectPropertyImpl("status", NS);
+            clazz.getDomain().add(OTRestClasses.RESTOperation());
+            clazz.getDomain().add(OTClasses.OpenToxResource());
+            ms_status = clazz;
+        }
+        return ms_status;
+    }
+
+//    public static OTObjectProperty uri() {
+//        if (ms_uri == null) {
+//            OTObjectProperty clazz = new OTObjectPropertyImpl("uri", NS);
+//            clazz.getDomain().add(OTRestClasses.RESTOperation());
+//            clazz.getDomain().add(OTRestClasses.RESTTemplate());
+//            ms_uri = clazz;
+//        }
+//        return ms_uri;
+//    }
 }
