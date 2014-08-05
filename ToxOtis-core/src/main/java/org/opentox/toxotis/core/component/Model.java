016efd8a6a05b1710c717e53ad658e049854b6af
philip <philip@147.102.82.233>
2014-08-05 09:02:21 +0300

    changed Model to set statistics

--- ToxOtis-core/src/main/java/org/opentox/toxotis/core/component/Model.java
+++ ToxOtis-core/src/main/java/org/opentox/toxotis/core/component/Model.java
@@ -129,7 +129,7 @@ public class Model extends OTOnlineResource<Model>
             FEAT_DIM = new int[]{150, 650},
             PARAM_DIM = new int[]{400, 150, 240},
             META_DIM = new int[]{150, 650};
-    
+    private Object statistics;
     /**
      * Create a new empty Model with a given URI.
      *
@@ -201,6 +201,22 @@ public class Model extends OTOnlineResource<Model>
         bos.close();
         byte[] data = bos.toByteArray();
         return data;
+    }
+
+    /**
+     * Gets statistics from weka for the model
+     */
+    public Object getStatistics() {
+        return statistics;
+    }
+
+    
+    /**
+     * Sets statistics from weka for the model
+     * @param statistics The object which can be used to store statistics.
+     */
+    public void setStatistics(Object statistics) {
+        this.statistics = statistics;
     }
 
     /**
